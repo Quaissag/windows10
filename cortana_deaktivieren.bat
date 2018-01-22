c2afae41da6f8ff7677477189867b540f0f2b925
Quaissag <30439133+manujeb@users.noreply.github.com>
2018-01-22 20:40:39 +0000

    Create cortana_deaktivieren.bat

---
+++ cortana_deaktivieren.bat
@@ -0,0 +1,6 @@
+REM Cortana in Windows 10 deaktivieren
+@echo off
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v AllowCortana /t REG_DWORD /d 0 /f
+taskkill /f /IM explorer.exe
+start explorer.exe
+exit
