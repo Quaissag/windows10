ee65b4ee05d5262193249efe6ce39f38bdf4ede8
Quaissag <30439133+manujeb@users.noreply.github.com>
2018-01-22 21:54:35 +0100

    Update cortana_deaktivieren.bat

--- cortana_deaktivieren.bat
+++ cortana_deaktivieren.bat
@@ -1,5 +1,8 @@
 @echo off
 REM Cortana in Windows 10 deaktivieren
+REM # Powershell Abfrage zum Ermitteln des Windows 10 Release
+REM #(Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\" -Name ReleaseID).ReleaseId #
+REM 
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v AllowCortana /t REG_DWORD /d 0 /f
 taskkill /f /IM explorer.exe
 start explorer.exe
