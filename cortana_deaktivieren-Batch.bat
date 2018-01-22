1e566cf1a0b2645d9d7fc0e24fb607a64ba3f002
Manuel J <30439133+Quaissag@users.noreply.github.com>
2018-01-23 00:42:04 +0100

    Update cortana_deaktivieren-Batch.bat

--- cortana_deaktivieren-Batch.bat
+++ cortana_deaktivieren-Batch.bat
@@ -6,12 +6,3 @@ taskkill /f /IM explorer.exe
 start explorer.exe
 
 exit
-
-
-REM für spätere Powershell-Variante 
-
-REM
-REM # Powershell Abfrage zum Ermitteln des Windows 10 Release
-REM #(Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\" -Name ReleaseID).ReleaseId #
-REM $registryPath = "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
-REM 
